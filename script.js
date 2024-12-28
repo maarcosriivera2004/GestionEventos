716b4311c9fa15e7cef98ea26fcf62fea6c26f7a
maarcosriivera2004 <marcosriveralm@gmail.com>
2024-12-28 02:19:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const hamburguesa = document.querySelector('#hamburguer')
-const enlaces = document.querySelector('#nav-links')
+document.addEventListener('DOMContentLoaded', () => {
+  const userIcon = document.querySelector('#user-icon');
+  const userOption = document.querySelector('#user-option');  // Selecciona el contenedor que envuelve el icono y el dropdown
+  const hamburguer = document.querySelector('#hamburguer');
+  const navLinks = document.querySelector('#nav-links');
 
-hamburguesa.addEventListener('click', () => {
-enlaces.classList.toggle("show")
-})
+  // Evento para mostrar/ocultar el dropdown del usuario
+  userIcon.addEventListener('click', () => {
+    console.log("Clic en el icono de usuario"); // Verifica que el evento se activa
+    userOption.classList.toggle('show-dropdown');  // Cambia la clase en el contenedor
+  });
+
+  // Evento para mostrar/ocultar el menú hamburguesa
+  hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
+  
+});
